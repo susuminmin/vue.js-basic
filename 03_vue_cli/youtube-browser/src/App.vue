8a82d7e628d1b8be03ec6dcddef6b86befb865c1
김수민 <mytoutbon@gmail.com>
2019-11-11 13:18:56 +0900

    Start vue-cli

---
+++ 03_vue_cli/youtube-browser/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- step 3 사용하기 -->
+    <SearchBar />
+  </div>
+</template>
+
+<script>
+// step 1
+import SearchBar from "./components/SearchBar.vue";
+
+// Vue
+export default {
+  name: "App", // Component 의 이름 지정
+  // step 2
+  components: {
+    SearchBar
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 03_vue_cli/youtube-browser/src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-on:input="onInput" /> <!-- event 등록 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      console.log(event.target.value)
+    }
+  }
+};
+</script>
+
+
+<style>
+</style>
