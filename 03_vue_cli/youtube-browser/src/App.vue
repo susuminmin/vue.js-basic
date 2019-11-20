5f436443e7cbc0c60b34e735c88368b81a4cd27a
김수민 <mytoutbon@gmail.com>
2019-11-20 13:22:22 +0900

    Finish vue-cli

--- 03_vue_cli/youtube-browser/src/App.vue
+++ 03_vue_cli/youtube-browser/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <!-- step 3 사용하기 -->
-    <SearchBar />
+    <SearchBar @inputChange="onInputChange" />
+    <!-- input 값 바뀔 때마다 발생하는 inputChange 이벤트 (내가 이름 지정) 발생하면 아래 정의한 onInputChange 실행시켜! -->
   </div>
 </template>
 
@@ -15,6 +16,13 @@ export default {
   // step 2
   components: {
     SearchBar
+  },
+  // 함수 정해서 SearchBar 로 넘겨줌
+  methods: {
+    // inputChange라고 하는 우리가 지정해 준 이벤트 발생할 때마다 실행
+    onInputChange: function(inputValue) {
+      console.log(inputValue);
+    }
   }
 };
 </script>
--- 03_vue_cli/youtube-browser/src/components/SearchBar.vue
+++ 03_vue_cli/youtube-browser/src/components/SearchBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <input type="text" v-on:input="onInput" /> <!-- event 등록 -->
+    <!-- input 값 바뀔 때마다 onInput 메서드 실행 -->
+    <input type="text" @input="onInput" />
   </div>
 </template>
 
@@ -9,7 +10,9 @@ export default {
   name: "SearchBar",
   methods: {
     onInput: function(event) {
-      console.log(event.target.value)
+      // <- 'inputChange' 라는 사용자 지정 이벤트 발생을 알리는 역할만 한다.
+      // SearchBar 역할 : input 값 바뀔 때 inputChange 이벤트 발생 --> 그 input 의 value 값 넘긴다.
+      this.$emit("inputChange", event.target.value);
     }
   }
 };
